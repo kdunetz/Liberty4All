8207dd6bb7b5e0831841e2b401dc8d3c3d392386
Kevin Dunetz <kdunetz@us.ibm.com>
2018-02-16 16:56:20 -0500

    keep working on it

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl delete -f liberty4all-deploy.yml
-kubectl delete -f liberty4all-svc.yml
-kubectl create -f liberty4all-deploy.yml
-kubectl create -f liberty4all-svc.yml
+kubectl delete -f liberty4all-deploy.yml -n default
+kubectl delete -f liberty4all-svc.yml -n default
+kubectl create -f liberty4all-deploy.yml -n default
+kubectl create -f liberty4all-svc.yml -n default
